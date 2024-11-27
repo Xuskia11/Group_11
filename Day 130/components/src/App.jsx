ed996661de478d98e764289f5e650ce81be8fc4a
Xuskia11 <atuka.09.123@gmail.com>
2024-11-27 20:00:56 +0400

    commit

---
+++ Day 130/components/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import TableRender from '../componentss/tables'
+
+function App() {
+  
+
+  return (
+    <div>
+      <TableRender names={["avto","erekle","data"]}/>
+    </div>
+  )
+}
+
+export default App
